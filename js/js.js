31bf99adb4944a934a5ac7990b6491faa9116276
raid4k <raid4k@gmail.com>
2020-01-30 17:53:13 +0300

    casino
    dasd

--- js/js.js
+++ js/js.js
@@ -1,2 +1,14 @@
   jQuery('.select2').SumoSelect({ selectAll: true});
 
+  jQuery('.s-casino-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    // autoplay:true,
+    fade: true,
+    cssEase: 'linear',
+    arrows:false,
+    
+    
+    
+  });
